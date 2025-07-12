bc48d2bc8988d6a5615f6d667c6fc2e648bb9b03
randytalla <randytala2@gmail.com>
2025-07-12 03:19:18 +0100

    created the blog app

--- FirstProject/Bloc_application/Bloc_application/settings.py
+++ FirstProject/Bloc_application/Bloc_application/settings.py
@@ -20,7 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-gjn*%8kaoh#+ge6d58qqyi89c50d-!p===#h08h-zjk=mkpll2'
+SECRET_KEY = (
+    'django-insecure-gjn*%8kaoh#+ge6d58qqyi89c50d-!p===#h08h-zjk=mkpll2'
+    )
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
