9c4ca73e5aabde55fcd110c3e33ce2030c3c067a
randytalla <randytala2@gmail.com>
2025-07-12 13:01:22 +0100

    Created app in my model and added it to setting.py

--- FirstProject/Bloc_application/blog/models.py
+++ FirstProject/Bloc_application/blog/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=250)
+    slug = models.SlugField(max_length = 250, unique=True)
+    body = models.TextField()
+    publish = models.DataTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add = True)
+    updated = models.DateField(auto_now = True)
+    
+    class Meta: ordering = ['-publish']
+    indexes = [models.Index(fields=['-publish'],)]
+
+    def __str__(self): 
+        return self.title
